cacecfbc5b320cf50d9d11eb5088e8cbf3a1e252
Sam Brozinsky <s77broz@aol.com>
2018-08-28 07:36:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function takeANumber(line, name) {
   return `Welcome, ${name}. You are number ${line.push(name)} in line.`;
 }
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+    
+  }
+}
